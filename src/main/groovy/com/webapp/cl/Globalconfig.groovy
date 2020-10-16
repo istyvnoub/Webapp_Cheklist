8cbc2381dfb62d3899a2abecee7b40a8f43ab4ea
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-10-16 11:54:13 +0200

    User Type -> Admin oder Regluar member

--- src/main/groovy/com/webapp/cl/Globalconfig.groovy
+++ src/main/groovy/com/webapp/cl/Globalconfig.groovy
@@ -1,4 +1,9 @@
 package com.webapp.cl
 
 class Globalconfig {
+
+    public static final def USER_TYPE =[
+            ADMINISTRATOR: "ADMINISTRATOR",
+            REGULAR_MEMBER: "REGULAR_MEMBER",
+    ]
 }
