ddb7e473bf484426cdd586f2bc91fa280eabebf7
Yvan Loic Noubom Woboyi <yvan.l.woboyi@stud.h-da.de>
2020-10-16 11:52:38 +0200

    Properites added

--- grails-app/domain/checklist/Member.groovy
+++ grails-app/domain/checklist/Member.groovy
@@ -1,16 +1,32 @@
 package checklist
 
+import com.webapp.cl.Globalconfig
+
 class Member {
+    String firstName
+    String lastName
+    String password
+    String customerType= Globalconfig.USER_TYPE.REGULAR_MEMBER
+    String identityHash
+    String email
 
+    Boolean isActivate= true
 
-
-
+     Date dateCreated
+    Date   lastUpdate
 
     //static hasMany = [task:Task,tasklist:Tasklist]
 
     static constraints = {
-
-
-
+        firstName blank: false
+        lastName blank: false
+        email nullable: true
+        identityHash nullable: true
+    }
+    def beforeInsert(){
+        this.password=this.password.encodeAsMD5()
+    }
+    def beforeUpdate(){
+        this.password=this.password.encodeAsMD5()
     }
 }
